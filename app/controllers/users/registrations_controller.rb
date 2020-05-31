a2a2a9abdb034d0e992c4e5c0638066977241fe9
waioli1223 <waioli1223@users.noreply.github.com>
2020-05-31 13:25:09 +0900

    update registration_controller
    ・コントローラないの記述を消すとひとつ前のコミットの状況（email以外全てエラーメッセージがでる）になるのでコントローラーの記述を変更しデータベースに保存できるよう修正する必要がある。
    ・なぜe-mailだけエラーメッセージが出ないのかわからない。データが飛んでいないならemailも入力がからというエラーメッセージが出るべき。
    ・ "commit"=>"登録する"}　もparamsに含まれているのが気になる。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,10 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  def create
+    User.save
+  end
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations', 
-    sessions: 'users/sessions'
   }
   root 'posts#index'
   resources :posts, only: [:index]
